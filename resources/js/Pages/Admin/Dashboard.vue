bbe8e137e2147bbfa82f4fedd3471586fc1900d9
dobanthony <dobanthony38@gmail.com>
2025-08-07 23:33:44 +0800

    deleted

--- resources/js/Pages/Admin/Dashboard.vue
+++ resources/js/Pages/Admin/Dashboard.vue
@@ -1,22 +1,10 @@
 <template>
-    <AdminLayout>
-        <div class="container mt-4">
-            <h1 class="mb-4">Admin Dashboard</h1>
-
-            <div class="card">
-            <div class="card-body">
-                <p>Welcome, {{ user.name }}!</p>
-                <p class="mb-0">Manage shops, inventory, and view analytics here.</p>
-            </div>
-            </div>
-        </div>
+  <AdminLayout>
+    <h1>Admin</h1>
+    <p>This uses a Vue layout with Inertia, responsive sidebar, and topbar.</p>
   </AdminLayout>
 </template>
 
 <script setup>
 import AdminLayout from '@/Layouts/AdminLayout.vue';
-import { usePage } from '@inertiajs/vue3'
-
-const { props } = usePage()
-const user = props.auth.user
 </script>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,5 @@
 <?php
 
-
-use App\Http\Controllers\Admin\ProductController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\RoleBasedDashboardController;
 use Illuminate\Foundation\Application;
@@ -35,16 +33,6 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
-
-Route::prefix('admin')->group(function () {
-    Route::get('/shops/{shop}/inventory', [ProductController::class, 'index'])->name('admin.inventory.index');
-    Route::get('/shops/{shop}/inventory/create', [ProductController::class, 'create'])->name('admin.inventory.create');
-
-    Route::post('/products', [ProductController::class, 'store'])->name('admin.products.store');
-    Route::get('/products/{product}/edit', [ProductController::class, 'edit'])->name('admin.products.edit');
-    Route::put('/products/{product}', [ProductController::class, 'update'])->name('admin.products.update');
-    Route::delete('/products/{product}', [ProductController::class, 'destroy'])->name('admin.products.destroy');
 });
 
 
